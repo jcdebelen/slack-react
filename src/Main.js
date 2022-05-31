a254e182b5df67e7ef6e436fca2648dd37292a4e
Ariel Estrada <estrada.ariel.m@gmail.com>
2022-05-31 19:20:17 +0800

    El: refactored required headers | WIP send & retrieve message

--- src/Main.js
+++ src/Main.js
@@ -1,35 +1,27 @@
-import React, { useEffect } from 'react'
-import Dashboard from './Dashboard'
-import App from './App'
-import { useState } from 'react'
+import React, { useEffect } from "react";
+import Dashboard from "./Dashboard";
+import App from "./App";
+import { useState } from "react";
 
 export default function Main() {
-    const [accessToken, setAccessToken] = useState('')
-    const [clientToken, setClientToken] = useState('')
-    const [expiry, setExpiry] = useState('')
-    const [uid, setUid] = useState('')
+  const [requiredHeaders, setRequiredHeaders] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
-    const [isAuthenticated, setIsAuthenticated] = useState(false)
-
-    useEffect(() => {
-        if (uid !== '') {
-            setIsAuthenticated(true)
-        } else if (uid === '') {
-            setIsAuthenticated(false)
-        }
-    }, [uid])
+  useEffect(() => {
+    if (requiredHeaders === false || requiredHeaders.accessToken === null) {
+      setIsAuthenticated(false);
+    } else {
+      setIsAuthenticated(true);
+    }
+  }, [requiredHeaders]);
 
   return (
     <div>
-        {isAuthenticated ? <Dashboard 
-        accessToken={accessToken} 
-        clientToken={clientToken} 
-        expiry={expiry} 
-        uid={uid} /> : <App 
-        setAccessToken={setAccessToken} 
-        setClientToken={setClientToken} 
-        setExpiry={setExpiry} 
-        setUid={setUid} />}
+      {isAuthenticated ? (
+        <Dashboard requiredHeaders={requiredHeaders} />
+      ) : (
+        <App setRequiredHeaders={setRequiredHeaders} />
+      )}
     </div>
-  )
+  );
 }
