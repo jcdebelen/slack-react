b8bfda21097fe0faf43904bf6ba7e01c005a7710
Ariel Estrada <estrada.ariel.m@gmail.com>
2022-05-31 19:20:17 +0800

    El: refactored required headers | WIP send & retrieve message

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,16 +1,15 @@
-import React from 'react'
-import { useRouteMatch } from 'react-router-dom';
-import Channels from './components/user/Channels';
-import Messages from './components/user/Messages';
+import React from "react";
+import { useRouteMatch } from "react-router-dom";
+import Channels from "./components/user/Channels";
+import Messages from "./components/user/Messages";
 
-export default function Dashboard({accessToken, clientToken, expiry, uid}) {
-    let match = useRouteMatch();
-
+export default function Dashboard({ requiredHeaders }) {
+  let match = useRouteMatch();
   return (
     <div>
       <h2>Dashboard</h2>
       <Channels />
-      <Messages />
+      <Messages requiredHeaders={requiredHeaders} />
     </div>
-  )
+  );
 }
--- src/components/non-user/Login.js
+++ src/components/non-user/Login.js
@@ -1,52 +1,79 @@
 import { Link } from "react-router-dom";
 import { useState } from "react";
 
-const Login = ({setAccessToken, setClientToken, setExpiry, setUid}) => {
-  const [emailAddress, setEmailAddress] = useState('')
-  const [password, setPassword] = useState('')
+const Login = ({ setRequiredHeaders }) => {
+  const [emailAddress, setEmailAddress] = useState("");
+  const [password, setPassword] = useState("");
 
   function login(email, password) {
     let data = {
       email: email,
-      password: password
-  }
-  
-  let requestOptions = {
-      method: 'POST',
-      body: JSON.stringify(data),
-      redirect: 'follow',
-      headers: {
-          'Content-Type': 'Application/json'
-      }
+      password: password,
     };
-    
+
+    let requestOptions = {
+      method: "POST",
+      body: JSON.stringify(data),
+      redirect: "follow",
+      headers: {
+        "Content-Type": "Application/json",
+      },
+    };
+
     fetch("http://206.189.91.54//api/v1/auth/sign_in", requestOptions)
-    .then(response => {
-      setAccessToken(response.headers.get('access-token')) /* access-token header */
-      setClientToken(response.headers.get('client')) /* client-token header */
-      setExpiry(response.headers.get('expiry')) /* expiry header */
-      setUid(response.headers.get('uid')) /* uid header */
-      return response.text()
+      .then((response) => {
+        let headers = {
+          accessToken: response.headers.get("access-token"),
+          clientToken: response.headers.get("client"),
+          expiry: response.headers.get("expiry"),
+          uid: response.headers.get("uid"),
+        };
+        setRequiredHeaders(
+          headers
+        ); /* object with access-token, client-token, expiry, and uid */
+        return response.text();
       })
-    .then(result => {
-      console.log(result)})
-    .catch(error => console.log('error', error));
+      .then((result) => {
+        let newResult = JSON.parse(result);
+
+        let id = newResult.data.id;
+
+        setRequiredHeaders((prevData) => {
+          let newData = prevData;
+          newData.currentUserId = id;
+          return newData;
+        });
+      })
+      .catch((error) => console.log("error", error));
   }
 
   return (
     <div id="main-con">
       <h1>Sign in to Slack</h1>
-      <form className="forms" onSubmit={e => {
-        e.preventDefault()
-        login(emailAddress, password)
-      }}>
+      <form
+        className="forms"
+        onSubmit={(e) => {
+          e.preventDefault();
+          login(emailAddress, password);
+        }}
+      >
         <div className="subform">
           <label>Email address</label>
-          <input type="text" className="input" value={emailAddress} onChange={e => setEmailAddress(e.target.value)}></input>
+          <input
+            type="text"
+            className="input"
+            value={emailAddress}
+            onChange={(e) => setEmailAddress(e.target.value)}
+          ></input>
         </div>
         <div className="subform">
           <label>Password</label>
-          <input type="password" className="input" value={password} onChange={e => setPassword(e.target.value)}></input>
+          <input
+            type="password"
+            className="input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></input>
         </div>
         <button type="submit" id="submit">
           Sign In
