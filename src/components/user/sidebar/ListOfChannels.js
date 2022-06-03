cd384ed5b600e2a9a3e159e18f9563cbeb42d260
JC De Belen <debelenjc@gmail.com>
2022-06-03 13:16:43 +0800

    designed ListOfChannels

--- src/components/user/sidebar/ListOfChannels.js
+++ src/components/user/sidebar/ListOfChannels.js
@@ -9,20 +9,22 @@ export default function ListOfChannels({ requiredHeaders }) {
   let [ids, setIds] = useState([]);
   let [channels, setChannels] = useState([]);
 
+  //Headers
   var myHeaders = new Headers();
-
-  var requestGet = {
-    method: "GET",
-    headers: myHeaders,
-    redirect: "follow",
-  };
-
   myHeaders.append("access-token", requiredHeaders.accessToken);
   myHeaders.append("client", requiredHeaders.clientToken);
   myHeaders.append("expiry", requiredHeaders.expiry);
   myHeaders.append("uid", requiredHeaders.uid);
   myHeaders.append("Content-Type", "Application/json");
 
+  //Get request
+  var requestGet = {
+    method: "GET",
+    headers: myHeaders,
+    redirect: "follow",
+  };
+
+  //Create Channel
   let createChannel = (newChannel, ids) => {
     let data = {
       name: newChannel,
@@ -47,7 +49,7 @@ export default function ListOfChannels({ requiredHeaders }) {
       })
       .catch((error) => console.log("error", error));
 
-    getChannels();
+    getChannelList();
   };
 
   let addMember = (newMember) => {
@@ -80,7 +82,7 @@ export default function ListOfChannels({ requiredHeaders }) {
   };
 
   // show channels
-  const getChannels = () => {
+  const getChannelList = () => {
     fetch("http://206.189.91.54//api/v1/channels", requestGet)
       .then((response) => response.text())
       .then((result) => {
@@ -93,181 +95,142 @@ export default function ListOfChannels({ requiredHeaders }) {
       .catch((error) => console.log("error", error));
   };
 
+  //prevent getChannelList from running every second
   useEffect(() => {
-    getChannels();
+    getChannelList();
   }, []);
 
+  // My Channels Component
   let MyChannels = () => {
-    return channels.map((channels, index) => (
-      <div className="channel-con" key={index}>
-        <button className="channel-name">{channels.name}</button>
-      </div>
-    ));
+    return (
+      <>
+        <div id="channel-title-con">
+          <img
+            src={`${process.env.PUBLIC_URL}arrowdown.png`}
+            alt="arrowdown"
+            className="arrowdown"
+          />
+          <div id="channel-title">Channels</div>
+        </div>
+
+        {channels.map((channels, index) => (
+          <div className="channel-con" key={index}>
+            <img
+              src={`${process.env.PUBLIC_URL}hash.png`}
+              alt="hash"
+              className="icon"
+            />
+            <div className="channel-name">{channels.name}</div>
+          </div>
+        ))}
+        <div className="channel-con">
+          <img
+            src={`${process.env.PUBLIC_URL}add.png`}
+            alt="add"
+            className="icon"
+          />
+          <div id="" onClick={() => setModal(true)} className="channel-name">
+            Add Channels
+          </div>
+        </div>
+      </>
+    );
   };
 
+  //output
   return (
     <>
-      <h4 id="channel-title">Channels</h4>
+      <div id="workspaces">
+        <h1 id="server">Avion School</h1>
+      </div>
       <MyChannels />
       {modal ? (
         <div>
-          <div className="c-sk-overlay">
-            <div
-              className="ReactModal__Content ReactModal__Content--after-open c-sk-modal c-sk-modal--fixed p-channel_create_modal"
-              tabIndex="-1"
-            >
-              <div className="c-sk-modal_header">
-                <div className="c-sk-modal_title_bar c-sk-modal_title_bar--pad_right p-channel_create_modal__header">
+          <div className="modal-overlay">
+            <div className="modal-box">
+              <div className="modal-header">
+                <div className="modal-title-bar">
                   <h1>Create a channel</h1>
                 </div>
               </div>
-              <div className="c-sk-modal_content p-channel_create_modal__content">
-                <div
-                  role="presentation"
-                  className="c-scrollbar c-scrollbar--hidden c-scrollbar--inherit_size"
-                >
-                  <div className="c-scrollbar__hider">
-                    <div className="c-scrollbar__child">
-                      <div className="c-sk-modal_content__inner">
-                        <div className="c-sk-modal_content_section">
-                          <div className="sk_dark_gray margin_bottom_150">
-                            Channels are where your team communicates. They’re
-                            best when organized around a topic — #marketing, for
-                            example.
-                          </div>
-                          <div>
-                            <label>
-                              <span className="p-channel_name_input__label_title">
-                                <strong>Add Members</strong>
-                              </span>
-                            </label>
-                            <div className="p-channel_name_input__input">
-                              <div
-                                className="c-search-select"
-                                data-qa="channel-name"
-                              >
-                                <div className="c-select_input__wrapper c-select_input--large c-select_input--with_icon_left p-channel_name_input__select_input_wrapper">
-                                  <div className="c-select_input__input_container p-channel_name_input__select_input_container">
-                                    <div data-qa-formtext="true">
-                                      <div className="c-input_text_icon c-input_text_icon--large">
-                                        <div
-                                          role="presentation"
-                                          className="c-input_character_count c-input_character_count--large"
-                                          data-qa="input_character_count"
-                                        >
-                                          <form
-                                            className="flexx"
-                                            onSubmit={(e) => {
-                                              e.preventDefault();
-                                              addMember(member);
-                                            }}
-                                          >
-                                            <input
-                                              className="c-input_text c-input_text--large c-input_text--with_icon c-select_input p-channel_name_input__select_input"
-                                              placeholder="email"
-                                              type="email"
-                                              value={member}
-                                              onChange={(e) =>
-                                                setMember(e.target.value)
-                                              }
-                                            />
-                                            <button
-                                              type="submit"
-                                              className="c-button c-button--primary c-button--medium"
-                                            >
-                                              add
-                                            </button>
-                                          </form>
-                                          <div className="flexx">
-                                            {members.map((email, index) => (
-                                              <p
-                                                key={index}
-                                                className="members"
-                                              >
-                                                <p className="boxx">{email},</p>
-                                              </p>
-                                            ))}
-                                          </div>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                  <div className="c-select_input__icon_container"></div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
-                          <div>
-                            <label>
-                              <span className="p-channel_name_input__label_title">
-                                <strong>Name</strong>
-                              </span>
-                              <span
-                                className="p-channel_name_input__label_error"
-                                data-qa="channel_name_error"
-                                role="alert"
-                              ></span>
-                            </label>
-                            <div className="p-channel_name_input__input">
-                              <div
-                                className="c-search-select"
-                                data-qa="channel-name"
-                              >
-                                <div
-                                  role="presentation"
-                                  className="c-select_input__wrapper c-select_input--large c-select_input--with_icon_left p-channel_name_input__select_input_wrapper"
-                                >
-                                  <div className="c-select_input__input_container p-channel_name_input__select_input_container">
-                                    <div data-qa-formtext="true">
-                                      <div className="c-input_text_icon c-input_text_icon--large">
-                                        <div
-                                          role="presentation"
-                                          className="c-input_character_count c-input_character_count--large"
-                                          data-qa="input_character_count"
-                                        >
-                                          <form
-                                            onSubmit={(e) => {
-                                              e.preventDefault();
-                                              createChannel(newChannel, ids);
-                                            }}
-                                          >
-                                            <input
-                                              className="c-input_text c-input_text--large c-input_text--with_icon c-select_input p-channel_name_input__select_input"
-                                              placeholder="e.g. plan-budget"
-                                              type="text"
-                                              value={newChannel}
-                                              onChange={(e) =>
-                                                setNewChannel(e.target.value)
-                                              }
-                                            />
-                                          </form>
-                                        </div>
-                                      </div>
-                                    </div>
-                                  </div>
-                                  <div className="c-select_input__icon_container"></div>
-                                </div>
-                              </div>
-                            </div>
-                          </div>
+              <div className="modal-middle">
+                <div className="modal-inner">
+                  <div className="modal-content-section">
+                    <div className="modal-text">
+                      Channels are where your team communicates. They’re best
+                      when organized around a topic — #marketing, for example.
+                    </div>
+                    <div>
+                      <label>
+                        <span className="modal-label">
+                          <strong>Name</strong>
+                        </span>
+                      </label>
+                      <div className="modal-channel-con">
+                        <form>
+                          <input
+                            className="modal-input"
+                            placeholder="e.g. plan-budget"
+                            type="text"
+                            value={newChannel}
+                            onChange={(e) => setNewChannel(e.target.value)}
+                          />
+                        </form>
+                      </div>
+                    </div>
+                    <div>
+                      <label>
+                        <span className="modal-label">
+                          <strong>Add Members</strong>
+                        </span>
+                      </label>
+                      <div className="modal-members-input-con">
+                        <form
+                          className="flex"
+                          onSubmit={(e) => {
+                            e.preventDefault();
+                            addMember(member);
+                          }}
+                        >
+                          <input
+                            className="modal-input"
+                            placeholder="email"
+                            type="email"
+                            value={member}
+                            onChange={(e) => setMember(e.target.value)}
+                          />
+                          <button type="submit" className="modal-button">
+                            add
+                          </button>
+                        </form>
+                        <div className="flex">
+                          {members.map((email, index) => (
+                            <p key={index} className="members">
+                              <p className="box">{email}</p>
+                            </p>
+                          ))}
                         </div>
                       </div>
                     </div>
                   </div>
                 </div>
               </div>
-              <div className="c-sk-modal_footer p-channel_create_modal__footer p-channel_create_modal__footer--paid_feature_badge">
-                <div className="c-sk-modal_footer_actions">
+              <div className="modal-footer-con">
+                <div className="modal-footer-button">
                   <button
-                    className="c-button c-button--primary c-button--medium c-button--disabled"
+                    className="modal-button"
                     type="button"
+                    onClick={(e) => {
+                      e.preventDefault();
+                      createChannel(newChannel, ids);
+                    }}
                   >
                     Create
                   </button>
                 </div>
               </div>
               <button
-                className="c-button-unstyled c-icon_button c-icon_button--light c-icon_button--size_medium c-sk-modal__close_button"
+                className="modal-close-button"
                 type="button"
                 onClick={() => setModal(false)}
               >
@@ -277,12 +240,7 @@ export default function ListOfChannels({ requiredHeaders }) {
           </div>
         </div>
       ) : (
-        <>
-          <button className="" onClick={() => setModal(true)}>
-            {" "}
-            Create Channel
-          </button>
-        </>
+        <></>
       )}
     </>
   );
